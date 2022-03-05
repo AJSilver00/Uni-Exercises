8ce27dcc7ce2adc7958b48fd3f0d355bd5ccbae7
AJSilver00 <100521051+AJSilver00@users.noreply.github.com>
2022-03-05 21:14:04 +0000

    Exercise 3

---
+++ Semester 1/Exercise 3/myturtlefunctions.py
@@ -0,0 +1,25 @@
+# Exercise set 3
+
+# Using the turtle module
+
+import turtle
+scrn = turtle.Screen()
+turt = turtle.Turtle()
+
+# 8. Experiment with the turtle environment
+turt.goto(100,50)
+turt.setheading(90)
+turt.forward(100)
+turt.penup()
+turt.forward(100)
+turt.pendown()
+turt.left(45)
+turt.fd(150)
+scrn.bye()
+
+# 9. Drawing a straight line
+def drawline (tt, direction, size):
+    tt = turtle
+    direction = int(direction)
+    size = int(size)
+    turt.tt
